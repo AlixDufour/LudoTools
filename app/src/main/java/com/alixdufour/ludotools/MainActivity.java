f05235288709c1c55c2e86ee5aa680dc1a14c423
Lonee19 <louise.benharira@gmail.com>
2022-11-13 22:19:04 -0500

    sort activity begining

--- app/src/main/java/com/alixdufour/ludotools/MainActivity.java
+++ app/src/main/java/com/alixdufour/ludotools/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, DiceActivity.class);
+                Intent i = new Intent(MainActivity.this, SortActivity.class);
                 startActivity(i);
 
             }
