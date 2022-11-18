93c5595a0e645b46e1bf12bab5cb23b4cec8cefd
Lonee19 <louise.benharira@gmail.com>
2022-11-18 17:05:46 -0500

    Revert "résolution bug partie 1"
    This reverts commit 3d039d53b02e241981c2b846e414971dff7ac61c.

---
+++ app/src/main/java/com/alixdufour/ludotools/SortActivity.java
@@ -0,0 +1,68 @@
+package com.alixdufour.ludotools;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class SortActivity extends AppCompatActivity {
+
+    Button buttonAdd;
+    Button buttonSort;
+    List player = new ArrayList();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sort);
+
+        buttonAdd = (Button) findViewById(R.id.AddButton);
+        buttonSort = (Button) findViewById(R.id.SortButton);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView nameEntered = findViewById(R.id.Name);
+                String name = nameEntered.getText().toString();
+                player.add(name);
+                TextView nameList = findViewById(R.id.ListNames);
+                String text = "";
+                int lenght = player.size();
+                for (int i = 0; i < lenght; i++) {
+                    text += (i+1) + "- " + player.get(i) + "\n";
+                }
+                nameList.setText(text);
+                nameEntered.setText("");
+            }
+        });
+
+        buttonSort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView nameList = findViewById(R.id.ListNames);
+                String text = "";
+                int lenght = player.size();
+                List indice = new ArrayList();
+                for (int i = 0; i < lenght; i++) {
+                    Random rng = new Random();
+                    int i1 = rng.nextInt(lenght);
+                    while (indice.contains(i1)){
+                        i1 = rng.nextInt(lenght);
+                    }
+                    indice.add(i1);
+                    text += (i+1) + "- " + player.get(i1) + "\n";
+                }
+                nameList.setText(text);
+            }
+        });
+    }
+}
