0c63254e4f0da76d530e8b6eeeb278c07cc66506
Lonee19 <louise.benharira@gmail.com>
2022-11-18 17:24:35 -0500

    création de l'activité

---
+++ app/src/main/java/com/alixdufour/ludotools/YamsActivity.java
@@ -0,0 +1,14 @@
+package com.alixdufour.ludotools;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YamsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yams);
+    }
+}
