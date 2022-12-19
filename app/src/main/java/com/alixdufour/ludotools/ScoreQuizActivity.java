261a5e1faa855797b90250233f996319e4c6e71b
Lonee19 <louise.benharira@gmail.com>
2022-12-18 21:47:08 -0500

    a

--- app/src/main/java/com/alixdufour/ludotools/ScoreQuizActivity.java
+++ app/src/main/java/com/alixdufour/ludotools/ScoreQuizActivity.java
@@ -92,6 +92,7 @@ public class ScoreQuizActivity extends AppCompatActivity {
                 });
 
 
+
                 builder.setNegativeButton("Annuler", new DialogInterface.OnClickListener(){
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
